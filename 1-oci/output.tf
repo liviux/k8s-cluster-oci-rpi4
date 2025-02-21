59e4851eae0f79b356d846636e1f5bab1619f1cb
Liviu Stefanescu <liviusa@gmail.com>
2025-02-21 17:33:20 +0200

    Refactor K3s configuration: consolidate common variables, enhance cloud-init scripts, and improve output descriptions

--- 1-oci/output.tf
+++ 1-oci/output.tf
@@ -1,4 +1,5 @@
 output "k3s_servers_ips" {
+  description = "Public IP addresses of K3s server nodes"
   depends_on = [
     data.oci_core_instance_pool_instances.k3s_servers_instances,
   ]
@@ -6,6 +7,7 @@ output "k3s_servers_ips" {
 }
 
 output "k3s_workers_ips" {
+  description = "Public IP addresses of K3s worker nodes"
   depends_on = [
     data.oci_core_instance_pool_instances.k3s_workers_instances,
   ]
@@ -13,9 +15,17 @@ output "k3s_workers_ips" {
 }
 
 output "public_lb_ip" {
+  description = "IP addresses of the public network load balancer"
   value = oci_network_load_balancer_network_load_balancer.k3s_public_lb.ip_addresses
 }
 
 output "k3s_extra_server_ip" {
+  description = "Public IP address of the extra K3s server node (if enabled)"
   value = var.k3s_extra_server_node ? oci_core_instance.k3s_extra_server_node[0].public_ip : null
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for your K3s cluster"
+  value       = "https://${local.public_lb_ip[0]}:${var.kube_api_port}"
+  sensitive   = true
 }
